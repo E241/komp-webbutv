e555d2c10b38b72f858dd0f121afcd98b2f86db3
Nibor9198 <robin.boregrim@gmail.com>
2015-05-12 11:12:35 +0200

    Startsidans animationer allt förutom onClick()

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 //* location.href='viewport.html *//
+$('#button').onClick(function() {click();});
 
+function click(){
+    $('#button').addClass('topbar');
+    setTimeout(click2, 2000)
+}
+function click2(){
+    $('#button').addClass('topbar2');
+    $('body').addClass("white");
+    $('h2').addClass('invis');
+    $('#back').addClass('invis');
+    $('h1').addClass('invis');
+    
+ //*   setTimeout(href, 2000)
+}
+function href(){
+    location.href='viewport.html';
+}
