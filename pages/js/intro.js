7473a7b580e34b2593618ddda666387777eff24f
Rickard Doverfelt <rickard@doverfelt.se>
2015-05-30 19:26:37 +0200

    Improving intro prewiew

--- pages/js/intro.js
+++ pages/js/intro.js
@@ -5,6 +5,19 @@ var mouse = {};
 var canvas = document.getElementById("jsCanvas");
 var context = canvas.getContext('2d');
 
+var counter = 0;
+var lastCount = 0;
+var currentColor = "#eeff00"
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 window.requestAnimFrame = (function () {
   return  window.requestAnimationFrame       || 
 		  window.webkitRequestAnimationFrame || 
@@ -23,13 +36,21 @@ function resizeCanvas() {
 
 //contex.clearRect(0,0,canvas.width,canvas.height);
 function moveRectTo(x, y, contex) {
-    contex.fillStyle = "#eeff00";
+    if (counter - 30 == lastCount) {
+        currentColor = getRandomColor();
+        lastCount = counter;
+    }
+    contex.fillStyle = currentColor;
     contex.fillRect(x-75/2, y-75/2, 75, 75);
+    counter++;
 }
 
 function drawBg(contex) {
     contex.fillStyle = "#000000";
     contex.fillRect(0, 0, canvas.width, canvas.height);
+    contex.fillStyle = "#ffffff";
+    contex.font = "18px sans-serif";
+    contex.fillText('Current color: ' + currentColor, 1, 18);
 }
 
 function mouseXY(e) {
